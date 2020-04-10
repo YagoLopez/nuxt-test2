fdb964d7f8faf76876d2ca6506a6292479acbc2f
Yago Lopez <ylopez@nalej.com>
2020-04-11 01:05:48 +0200

    Add dynamic body and title text

--- components/Posts.vue
+++ components/Posts.vue
@@ -15,16 +15,14 @@
           <v-list-item>
             <v-list-item-avatar color="grey"></v-list-item-avatar>
             <v-list-item-content>
-              <v-list-item-title class="headline">Our Changing Planet</v-list-item-title>
+              <v-list-item-title class="headline">{{ title.title }}</v-list-item-title>
               <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
 
           <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"/>
 
-          <v-card-text>
-            Visit ten places on our planet that are undergoing the biggest changes today.
-          </v-card-text>
+          <v-card-text>{{ title.body }}</v-card-text>
 
           <v-card-actions>
             <v-btn text color="deep-purple accent-4">Read</v-btn>
