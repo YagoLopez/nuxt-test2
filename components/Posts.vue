a1d972b24de98c67a801a208b3b17c7ef652c069
Yago Lopez <ylopez@nalej.com>
2020-04-11 13:29:02 +0200

    Refactor titles name for posts name
    And other refactors

--- components/Posts.vue
+++ components/Posts.vue
@@ -1,18 +1,19 @@
 <template>
   <v-card flat>
     <v-layout row wrap>
-      <v-flex v-for="(title, index) in titles" :key="index">
+      <v-flex v-for="(post, index) in posts" :key="index">
 
+        <!--todo: hacer post component-->
         <v-card max-width="344" class="mx-auto">
           <v-list-item>
             <v-list-item-avatar color="grey"></v-list-item-avatar>
             <v-list-item-content>
-              <v-list-item-title class="headline">{{ title.title }}</v-list-item-title>
+              <v-list-item-title class="headline">{{ post.title }}</v-list-item-title>
               <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
           <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"/>
-          <v-card-text>{{ title.body }}</v-card-text>
+          <v-card-text>{{ post.body }}</v-card-text>
           <v-card-actions>
             <v-btn text color="deep-purple accent-4">Read</v-btn>
             <v-btn text color="deep-purple accent-4">Bookmark</v-btn>
@@ -35,17 +36,19 @@
   @Component
   export default class Posts extends Vue {
 
-    titles: Object[] = [];
+    // todo: improve posts and post type
+    // todo: create post repository for separation of concerns
+    posts: Object[] = [];
     page: number = 1;
 
     get url() {
-      return "https://jsonplaceholder.typicode.com/posts?_page=" + this.page;
+      return `https://jsonplaceholder.typicode.com/posts?_page=${this.page}`;
     }
 
     async fetchData() {
         const response = await axios.get(this.url);
-        this.titles = response.data;
-        console.log('this.titles', this.titles);
+        this.posts = response.data;
+        // console.log('this.posts', this.posts);
         console.log('this.page', this.page);
         console.log('this.url', this.url);
     }
@@ -57,11 +60,10 @@
     infiniteScroll($state: any) {
       setTimeout(() => {
         this.page++;
-        // todo: use async/await
         axios.get(this.url)
           .then((response) => {
             if (response.data.length > 1) {
-              response.data.forEach((item: any) => this.titles.push(item));
+              response.data.forEach((item: any) => this.posts.push(item));
               $state.loaded();
             } else {
               $state.complete()
